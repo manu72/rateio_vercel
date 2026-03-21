326d385f501064356ad7c9d21e9be292f09d2cb5
Manu Hume <manuhume@hotmail.com>
2026-03-21 11:09:34 +1100

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,7 +21,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-slate-100 dark:bg-slate-950 min-h-screen`}>{children}</body>
+      <body className={`${inter.className} bg-slate-100 dark:bg-slate-950 min-h-screen`}>
+          {children}
+          <Analytics />
+        </body>
     </html>
   );
 }
